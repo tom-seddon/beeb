6ba24517f88211c5e827ed17548d9a84ea0392f7
Tom Seddon <tom@tmbp>
2018-07-27 02:51:50 +0100

    Add text2bbc. DOS (CR+LF)/Unix (LF) -> BBC (CR) line endings.

---
+++ text2bbc.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import os,sys,argparse
+
+##########################################################################
+##########################################################################
+
+def main(options):
+    for file_name in options.file_names:
+        with open(file_name,'rt') as f: data=f.read()
+        data=data.replace('\n','\r')
+        with open(file_name,'wb') as f: f.write(data)
+
+##########################################################################
+##########################################################################
+
+if __name__=='__main__':
+    parser=argparse.ArgumentParser(description='convert text file to BBC Micro line endings')
+
+    parser.add_argument('file_names',
+                        metavar='FILE',
+                        nargs='+',
+                        help='file(s) to convert')
+
+    main(parser.parse_args(sys.argv[1:]))
+    
