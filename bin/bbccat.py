26e995e5f7e1394ab7e4a50fe074b784146c3f02
Tom Seddon <tom@>
2024-01-12 00:30:27 +0000

    Add basic handling for CHR$127.

--- bin/bbccat.py
+++ bin/bbccat.py
@@ -6,13 +6,24 @@ def main2(options):
         with open(path,'rb') as f: data=f.read()
         line=''
         i=0
-        while i<len(data):
-            # Skip LF in the LF CR line ending
-            if data[i]==10 and i<len(data) and data[i+1]==13: i+=1
+
+        def cr(n):
+            nonlocal i
+            nonlocal line
             
-            if data[i]==13:
-                print(line)
-                line=''
+            i+=n-1
+            print(line)
+            line=''
+        
+        while i<len(data):
+            # Skip any line ending that could plausibly be appropriate
+            # for the BBC Micro: so CR, CR LF or LF CR.
+
+            if data[i:i+2]==b'\r\n' or data[i:i+2]==b'\n\r': cr(2)
+            elif data[i]==13: cr(1)
+            elif data[i]==127 and options.handle_delete:
+                # try to do roughly the right thing
+                if len(line)>0: line=line[:-1]
             elif data[i]<32 or data[i]>126: line+='\\x%02X'%data[i]
             else: line+=chr(data[i])
             i+=1
@@ -21,6 +32,7 @@ def main2(options):
 
 def main(argv):
     parser=argparse.ArgumentParser(description='print BBC Micro text file to stdout')
+    parser.add_argument('-d','--handle-delete',action='store_true',help='''(try to) handle CHR$127''')
     parser.add_argument('paths',metavar='FILE',nargs='+',help='file(s) to print')
 
     main2(parser.parse_args(argv))
