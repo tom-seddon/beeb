d3218a78909819277521727eeafcad67dd27619e
Tom Seddon <tom@tmbp>
2025-03-23 15:47:38 +0000

    Fix up convert_ddtmass.

--- bin/convert_ddtmass.py
+++ bin/convert_ddtmass.py
@@ -17,22 +17,8 @@ import sys,os,os.path,argparse,collections
 ##########################################################################
 ##########################################################################
 
-# TODO: the label fixup is completely wrong! - but it didn't mattec
-# the code I tested it with enough to actually fix it. Needs to try to
-# xref things a bit better. Don't rename the global symbol. Check
-# caller and callee. Is the reference the global symbol? Act
-# accordingly.
-
-# TODO: seems like it might accept BASIC-type :-sepraated multiple
-# instructions per line, not currently handled.
-
 # TODO: should fix up labels such as "x" (etc.) that would trigger the
 # shadow definition of built-in warning
-
-# TODO: @E "fixup" should ignore @E in strings
-
-# TODO: comment alignment ain't right. Lines with only comments
-# sometimes seem to end up at COMMENT_COLUMN rather than column 0
 
 ##########################################################################
 ##########################################################################
@@ -112,27 +98,20 @@ def read(input_path,name,lines,indent=''):
             data=bytearray(data)
             data.append(13)
 
-    line_no=1
-    begin=0
-    while begin<len(data):
-        end=data.find(13,begin)
-        assert end!=-1
+    line_index=0
+    line_begin=0
+    while line_begin<len(data):
+        line_end=data.find(13,line_begin)
+        assert line_end!=-1
 
-        # no - strings may contain embedded control chars
-        
-        # for i in range(begin,end):
-        #     if data[i]<32 or data[i]>=126:
-        #         fatal('%s:%d:%d: bad char: %d'%(input_path,line_no,1+i-begin,data[i]))
+        line_bytes=data[line_begin:line_end]
+        line_number=line_index+1
+        line_index+=1
 
-        line=Line(file=file,
-                  number=line_no,
-                  bytes=data[begin:end])
-        line_no+=1
-
-        begin=end+1
+        line_begin=line_end+1
 
         # handle any directives that need doing immediately.
-        sline=line.bytes.strip()
+        sline=line_bytes.strip()
         include_file=get_suffix(sline,b'$INCLUDE ')
         if include_file is not None:
             # doesn't check .inf files. Assumes on-disk naming as per
@@ -146,16 +125,39 @@ def read(input_path,name,lines,indent=''):
             #
             # oh well.
             lines.append(Line(file=file,
-                              number=line_no,
+                              number=line_number,
                               bytes=b'; include_start: '+include_file))
             read(input_path2,include_file.decode('ascii'),lines,indent+'  ')
             lines.append(Line(file=file,
-                              number=line_no,
+                              number=line_number,
                               bytes=b'; include_end: '+include_file))
             continue
 
-        # add line to the list.
-        lines.append(line)
+        # split line into parts?
+        in_string=False
+        stmt_begin=0
+        stmt_end=0
+        any=False
+        while stmt_end<len(line_bytes):
+            if line_bytes[stmt_end]==ord('"'):
+                in_string=not in_string
+                stmt_end+=1
+            elif line_bytes[stmt_end]==ord(':') and not in_string:
+                lines.append(Line(file=file,
+                                  number=line_number,
+                                  bytes=line_bytes[stmt_begin:stmt_end]))
+                any=True
+                stmt_end+=1
+                stmt_begin=stmt_end
+            else: stmt_end+=1
+
+        if stmt_begin!=stmt_end:
+            lines.append(Line(file=file,
+                              number=line_number,
+                              bytes=line_bytes[stmt_begin:stmt_end]))
+        elif not any: lines.append(Line(file=file,
+                                        number=line_number,
+                                        bytes=line_bytes))
 
 ##########################################################################
 ##########################################################################
@@ -219,7 +221,7 @@ def fix_up_label_references(text,old_label,new_label):
 
 # Split each line into its constituent parts: label, label value,
 # code, comment.
-def split_lines(lines,locations_by_label):
+def split_lines(lines,lines_by_label):
     # Split each line into its parts.
     for line in lines:
         def fatal_line(msg,i=None):
@@ -260,10 +262,10 @@ def split_lines(lines,locations_by_label):
                 line.label_is_global=True
             else: line.label_is_global=False
 
-            locations_by_label.setdefault(line.label,[]).append(
-                Location(file=line.file,
-                         line=line.number,
-                         column=None))
+            lines_by_label.setdefault(line.label,[]).append(line)
+                # Location(file=line.file,
+                #          line=line.number,
+                #          column=None))
 
         skip_spaces()
 
@@ -294,7 +296,9 @@ def split_lines(lines,locations_by_label):
                 fatal_line('unterminated string')
 
             content=l[value_begin:value_end].rstrip()
-            if is_code: line.code=content
+            if is_code:
+                if len(content.strip())==0: content=None
+                line.code=content
             else: line.label_value=content
             
             i=value_end
@@ -312,31 +316,32 @@ def split_lines(lines,locations_by_label):
 # output, so duplicated local names will need uniquifying.
 #
 # The result is ugly, but oh well.
-def fix_up_duplicated_labels(lines,locations_by_label):
-    for label,locations in locations_by_label.items():
-        if len(locations)==1: continue # not duplicated
+
+LabelRename=collections.namedtuple('LabelRename','new_name file')
+
+def fix_up_duplicated_labels(lines,lines_by_label):
+    for label,label_lines in lines_by_label.items():
+        if len(label_lines)==1: continue # not duplicated
         
-        for i in range(len(locations)):
-            for j in range(i+1,len(locations)):
-                if locations[i].file is locations[j].file:
-                    fatal('unsupported duplicate label: %s'%label)
+        for i in range(len(label_lines)):
+            for j in range(i+1,len(label_lines)):
+                if label_lines[i].file is label_lines[j].file:
+                    fatal('label duplicated in same file: %s'%label)
 
-        old_name=label
-        for location_index,location in enumerate(locations):
-            new_name=old_name+'_%d'%location_index
-            for line in lines:
-                if not (line.file is location.file): continue
-
-                # Fix up the label definition itself.
-                if line.label==old_name: line.label=new_name
-
-                # Fi up actual code.
-                line.code=fix_up_label_references(line.code,
-                                                  old_name,
-                                                  new_name)
-                line.label_value=fix_up_label_references(line.label_value,
-                                                         old_name,
-                                                         new_name)
+        rename_index=0
+        for label_line in label_lines:
+            if not label_line.label_is_global:
+                # rename occurrences of this label in the same file
+                label_line.label='%s_%d'%(label_line.label,rename_index)
+                rename_index+=1
+                for line in lines:
+                    if line.file is label_line.file:
+                        line.code=fix_up_label_references(line.code,
+                                                          label,
+                                                          label_line.label)
+                        line.label_value=fix_up_label_references(line.label_value,
+                                                                 label,
+                                                                 label_line.label)
 
 ##########################################################################
 ##########################################################################
@@ -391,6 +396,7 @@ def fix_up_assembler_oddities(lines):
                     line.code=(line.code[:-len(indexing_suffix)]+
                                indexing_suffix.lower())
 
+            # Could be cleverer.
             if line.code.endswith(b' A'): line.code=line.code[:-2]+b' a'
 
         def fix_stuff(text):
@@ -403,7 +409,7 @@ def fix_up_assembler_oddities(lines):
                          text[i+1]>=ord('a') and text[i+1]<=ord('f'))):
                         text=text[:i]+b'$'+text[i+1:]
 
-                text=text.replace(b'@E',b'*')
+                text=fix_up_operator_references(text,b'@E',b'*')
                 text=fix_up_operator_references(text,b'MOD',b'%')
                 text=fix_up_operator_references(text,b'EOR',b'^')
 
@@ -509,6 +515,9 @@ def write_output(lines,f):
             text+=line.code.decode('ascii')
 
         if line.comment is not None:
+            # if 'ELECTRON BASED' in line.comment:
+            #     print('%s:%d'%(line.file.path,line.number))
+            #     print('  got label=%d; got code=%d'%(line.label is not None,line.code is not None))
             if len(text)>0: column(COMMENT_COLUMN)
             text+='; %s'%line.comment
 
@@ -524,10 +533,10 @@ def main2(options):
     read(options.input_path,None,lines)
     pv('read %d lines\n'%len(lines))
 
-    locations_by_label={}
-    split_lines(lines,locations_by_label)
+    lines_by_label={}
+    split_lines(lines,lines_by_label)
 
-    fix_up_duplicated_labels(lines,locations_by_label)
+    fix_up_duplicated_labels(lines,lines_by_label)
 
     fix_up_non_ascii_chars(lines)
 
